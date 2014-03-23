65545e434de33dc4a955b4a03189d102c7cdc824
Grace Chen <grace.s.chen@duke.edu>
2014-03-23 14:49:55 -0400

    cleaned up MainActivity class

--- src/com/example/weatherapp/MainActivity.java
+++ src/com/example/weatherapp/MainActivity.java
@@ -47,94 +47,10 @@ import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks,
 GooglePlayServicesClient.OnConnectionFailedListener, OnEditorActionListener {
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-//	private final static int
-//    CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-//	Location mLocation;
-//	
-//	public static class ErrorDialogFragment extends DialogFragment {
-//        // Global field to contain the error dialog
-//        private Dialog mDialog;
-//        // Default constructor. Sets the dialog field to null
-//        public ErrorDialogFragment() {
-//            super();
-//            mDialog = null;
-//        }
-//        // Set the dialog to display
-//        public void setDialog(Dialog dialog) {
-//            mDialog = dialog;
-//        }
-//        // Return a Dialog to the DialogFragment.
-//        @Override
-//        public Dialog onCreateDialog(Bundle savedInstanceState) {
-//            return mDialog;
-//        }
-//    }
-//    /*
-//     * Handle results returned to the FragmentActivity
-//     * by Google Play services
-//     */
-//    @Override
-//    protected void onActivityResult(
-//            int requestCode, int resultCode, Intent data) {
-//        // Decide what to do based on the original request code
-//        switch (requestCode) {
-//            
-//            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
-//            /*
-//             * If the result code is Activity.RESULT_OK, try
-//             * to connect again
-//             */
-//                switch (resultCode) {
-//                    case Activity.RESULT_OK :
-//                    /*
-//                     * Try the request again
-//                     */
-//                   
-//                    break;
-//                }
-//            
-//        }
-//     }
-//    
-//    private boolean servicesConnected() {
-//        // Check that Google Play services is available
-//        int resultCode =
-//                GooglePlayServicesUtil.
-//                        isGooglePlayServicesAvailable(this);
-//        // If Google Play services is available
-//        if (ConnectionResult.SUCCESS == resultCode) {
-//            // In debug mode, log the status
-//            Log.d("Location Updates",
-//                    "Google Play services is available.");
-//            // Continue
-//            return true;
-//        // Google Play services was not available for some reason
-//        } else {
-//            // Get the error code
-//            // Get the error dialog from Google Play services
-//            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-//                    resultCode,
-//                    this,
-//                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
-//
-//            // If Google Play services can provide an error dialog
-//            if (errorDialog != null) {
-//                // Create a new DialogFragment for the error dialog
-//                ErrorDialogFragment errorFragment =
-//                        new ErrorDialogFragment();
-//                // Set the dialog in the DialogFragment
-//                errorFragment.setDialog(errorDialog);
-//                // Show the error dialog in the DialogFragment
-//                //fixed it by changing to getFragmentManager .... acc
-//               errorFragment.show(getFragmentManager(),"Location Updates");
-//            }
-//        }
-//		return false;
-//    }
-//	 LocationClient mLocationClient = new LocationClient(this, this, this);
 	
-	 protected void onCreate(Bundle savedInstanceState) {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -149,9 +65,7 @@ GooglePlayServicesClient.OnConnectionFailedListener, OnEditorActionListener {
 		    }
 		});
 		// Get a handle to the Map Fragment
-		GoogleMap map = ((MapFragment) getFragmentManager()
-				.findFragmentById(R.id.map)).getMap();
-//mLocation = mLocationClient.getLastLocation();
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		LatLng sydney = new LatLng(-33.867, 151.206);
 		LatLng UNC = new LatLng(35.9083, -79.0500);
 		LatLng USM = new LatLng(39.9139, 116.3917);
@@ -204,91 +118,38 @@ GooglePlayServicesClient.OnConnectionFailedListener, OnEditorActionListener {
                 0, 0, mlocListener); 
 	}
 
-//	 public void sendMessage(View view) {
-//		 Intent intent = new Intent(this, DisplayMessageActivity.class);
-//		 EditText editText = (EditText) findViewById(R.id.edit_message);
-//		 String message = editText.getText().toString();
-//		 intent.putExtra(EXTRA_MESSAGE, message);		}
-@Override
-public void onConnectionFailed(ConnectionResult result) {
-	// TODO Auto-generated method stub
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// TODO Auto-generated method stub
 	
-}
+	}
 
-@Override
-public void onConnected(Bundle connectionHint) {
-	// TODO Auto-generated method stub
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		// TODO Auto-generated method stub
 	
-}
+	}
 
-@Override
-public void onDisconnected() {
-	// TODO Auto-generated method stub
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
 	
-}
+	}
 
-public void onFailure(int reason) {
-	// TODO Auto-generated method stub
-	
-}
+	public void onFailure(int reason) {
+		// TODO Auto-generated method stub
+		
+	}
 
-public void onSuccess() {
-	// TODO Auto-generated method stub
+	public void onSuccess() {
+		// TODO Auto-generated method stub
 	
-}
+	}
 
-@Override
-public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-	// TODO Auto-generated method stub
-	return false;
-}
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
-//	 protected void onStart() {
-//	        super.onStart();
-//	        // Connect the client.
-//	        mLocationClient.connect();
-//	    }
-//	   
-//	    @Override
-//	    protected void onStop() {
-//	        // Disconnecting the client invalidates it.
-//	        mLocationClient.disconnect();
-//	        super.onStop();
-//	    }
-//
-//		@Override
-//		public void onConnectionFailed(ConnectionResult connectionResult) {
-//			 if (connectionResult.hasResolution()) {
-//		            try {
-//		                // Start an Activity that tries to resolve the error
-//		                connectionResult.startResolutionForResult(
-//		                        this,
-//		                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-//		                /*
-//		                 * Thrown if Google Play services canceled the original
-//		                 * PendingIntent
-//		                 */
-//		            } catch (IntentSender.SendIntentException e) {
-//		                // Log the error
-//		                e.printStackTrace();
-//		            }
-//		        } else {
-//		            /*
-//		             * If no resolution is available, display a dialog to the
-//		             * user with the error.
-//		             */
-//		            showDialog(connectionResult.getErrorCode());
-//		        }			
-//		}
-//
-//		@Override
-//		public void onConnected(Bundle connectionHint) {
-//			Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();			
-//		}
-//
-//		@Override
-//		public void onDisconnected() {
-//			Toast.makeText(this, "Disconnected. Please re-connect.",
-//	                Toast.LENGTH_SHORT).show();			
-//		}
 }
